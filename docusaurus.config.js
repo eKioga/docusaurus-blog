1a924999a689d62a98d8ea60e520c371c40d5526
Kioga <ericpost@protonmail.com>
2025-12-03 18:52:26 -0800

    eric-post.com is working! But, base URL has it all broken. Trying to fix that here.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://ekioga.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-blog/',
+  baseUrl: '/eric-post.com/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
